31d36104d59ea5f8aff5fbcf87da78be661d9cc3
Dhanshri Pawade <pd8806562875@gmail.com>
2025-03-24 16:06:29 +0530

    change margin

--- loginfrom/src/components/SidebarPages/User.jsx
+++ loginfrom/src/components/SidebarPages/User.jsx
@@ -35,11 +35,11 @@ function User() {
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
-        <div className='d-flex mt-5'>
+        <div className='d-flex '>
             <div className='col-2'>
                 <Sidebar />
             </div>
-            <div className='col-10'>
+            <div className='col-10 mt-5'>
                 <Navbar1 />
                 <div className='d-flex justify-content-between align-items-center mt-4 mx-4'>
                     <h4>Users</h4>
